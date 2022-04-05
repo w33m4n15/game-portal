39db38e611485149c199e3d4e85e622162aceadd
Eben Coetzer <eben@aves.co.za>
2022-04-05 14:26:36 +0200

    super admin page started

---
+++ src/app.js
@@ -0,0 +1,25 @@
+"enable-strict";
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "1234567890",
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log("Error connecting to the MySQL Database");
+    return;
+  }
+  console.log("Connection established sucessfully");
+});
+connection.end((error) => {});
+
+userAccount = [
+  (userName = ""),
+  (surname = ""),
+  (email = ""),
+  (number = ""),
+  (password = ""),
+];
