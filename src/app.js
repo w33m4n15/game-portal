17dc5c6716fd6244ff63d98f47d76dddca5f1051
Eben Coetzer <eben@aves.co.za>
2022-04-08 11:35:52 +0200

    App.js edited with SQL connection request

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "user",
-  password: "1234567890",
+  user: "eben",
+  password: "lsql!@#2025!@#EB",
+  database: "shaGAME01",
 });
 
 connection.connect((error) => {
@@ -14,4 +15,4 @@ connection.connect((error) => {
   }
   console.log("Connection established sucessfully");
 });
-connection.end((error) => {});
+// connection.end((error) => {});
